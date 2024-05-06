704d9733b8363624682cf122557272970066d6d0
juba <juliano.pedraca@gmail.com>
2024-05-06 00:59:35 -0300

    retirado console logs

--- src/_config/database.config.ts
+++ src/_config/database.config.ts
@@ -1,16 +1,6 @@
 import { registerAs } from "@nestjs/config";
 
-export default registerAs('database', () => {
-    console.log('--------------------------- ENV ----------------------------');
-    console.log(process.env.DB_USERNAME);
-    console.log(process.env.DB_PORT);
-    console.log(process.env.DB_USERNAME);
-    console.log(process.env.DB_PASSWORD);
-    console.log(process.env.DB_NAME);
-    console.log(process.env.NODE_ENV === 'development');
-    console.log( process.env.NODE_ENV === 'development');
-    console.log('--------------------------- FIM ENV ----------------------------');
-    return ({
+export default registerAs('database', () => ({
     type: 'postgres',
     host: process.env.DB_USERNAME || 'postgres',
     port: process.env.DB_PORT || 5000,
@@ -22,6 +12,4 @@ export default registerAs('database', () => {
     migrations: [`${__dirname}/../../db/**/*{.ts,.js}`],
     autoLoadEntities: true,
     migrationsTableName: 'migrations',
-})}
-
-);
+}));
